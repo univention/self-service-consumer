b0aa0c24feee019ff8439de9bdb3d12037e00f0b
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-16 05:28:21 +0200

    test: Add common behavior tests
    Issue: univention/dev/internal/team-nubus#892

---
+++ tests/chart/common_behavior/test_image_configuration.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from univention.testing.helm.best_practice.image_configuration import ImageConfiguration
+from univention.testing.helm.utils import apply_mapping
+
+
+class TestImageConfiguration(ImageConfiguration):
+
+    def adjust_values(self, values: dict):
+        mapping = {
+            "waitForDependency.image": "image",
+        }
+        apply_mapping(values, mapping, copy=True)
+        return values
